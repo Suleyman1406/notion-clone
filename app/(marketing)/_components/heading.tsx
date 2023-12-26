24fe5c1a11a69650a7dd992453ce617117205ffd
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-12-26 23:04:33 +0400

    feat: navbar & hero section configured for auth user

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,15 @@
 "use client";
-
-import { Button } from "@/components/ui/button";
+import { useConvexAuth } from "convex/react";
 import { ArrowRightIcon } from "lucide-react";
 
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+import { SignInButton } from "@clerk/clerk-react";
+
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -13,10 +19,27 @@ export const Heading = () => {
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         Danotion is the connected workspace where <br /> better work happens.
       </h3>
-      <Button>
-        Enter Danotion
-        <ArrowRightIcon className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="flex w-full items-center justify-center h-10">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {!isLoading && isAuthenticated && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Danotion
+            <ArrowRightIcon className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isLoading && !isAuthenticated && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Danotion Free
+            <ArrowRightIcon className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,13 +1,19 @@
 "use client";
 
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import Link from "next/link";
+
 import { useScrolledTop } from "@/hooks/use-scrolled-top";
 import { cn } from "@/lib/utils";
 
-import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
 import { Logo } from "./logo";
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const isScrolled = useScrolledTop();
 
   return (
@@ -19,7 +25,28 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-        <Button>Login</Button>
+        {isLoading ? (
+          <Spinner />
+        ) : isAuthenticated ? (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href={"/documents"}>Enter Danotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        ) : (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Login
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Danotion Free</Button>
+            </SignInButton>
+          </>
+        )}
+
         <ThemeToggle />
       </div>
     </div>
