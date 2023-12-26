d06fa714a8815ae1e49e4a3dda7ab04d75e594ee
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-12-27 00:40:58 +0400

    feat: user dropdown added to sidebar

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,38 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+
 const DocumentsPage = () => {
-  return <div>This is protected page</div>;
+  const { user } = useUser();
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        width={300}
+        height={300}
+        alt="Empty Documents Page"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        width={300}
+        height={300}
+        alt="Empty Dark Documents Page"
+        className="dark:block hidden"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Danotion!
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create Note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsPage;
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import { cn } from "@/lib/utils";
+import { ElementRef, useEffect, useRef, useState } from "react";
 import { ChevronsLeftIcon, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
-import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+
+import { cn } from "@/lib/utils";
+
+import { UserItem } from "./user-item";
 
 export const Navigation = () => {
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -105,7 +108,7 @@ export const Navigation = () => {
           <ChevronsLeftIcon className="h-6 w-6" />
         </div>
         <div>
-          <p className="whitespace-nowrap">Action Items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
