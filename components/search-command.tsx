e8addafc565b113a7f803c77a521b110d78897c9
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-12-31 04:34:22 +0400

    fix: redirect from search url fixed

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -41,7 +41,7 @@ export const SearchCommand = () => {
   }, [toggle]);
 
   const onSelect = (id: string) => {
-    router.push(`/documents/${id}`);
+    router.push(`/documents/${id.split("-")[0]}`);
     onClose();
   };
 
